cc6f128e8f5daab1ca3f8065352dadd417190145
Talha <mtalha66255@gmail.com>
2021-03-26 11:56:11 +0500

    My first commit of program

---
+++ TalhasApplication/TalhasApplication/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TalhasApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myFirstName, myLastName;
+            Console.WriteLine("What is your name ?");
+            Console.Write("Enter your first name = ");
+            myFirstName = Console.ReadLine();
+
+            Console.Write("Enter your last name = ");
+            myLastName = Console.ReadLine();
+            Console.WriteLine("Your name is = " + myFirstName + " " + myLastName);
+            Console.WriteLine("Thank you!" + " " + myFirstName + " " + myLastName);
+
+        }
+    }
+}
