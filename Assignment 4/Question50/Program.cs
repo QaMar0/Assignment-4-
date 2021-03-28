c3fcb54f0426ff5f47122b2aa221c0197e4530a2
Talha <mtalha66255@gmail.com>
2021-03-28 21:09:43 +0500

    Made changes in QUESTION50 Part H and J

--- Assignment 4/Question50/Program.cs
+++ Assignment 4/Question50/Program.cs
@@ -6,6 +6,7 @@ namespace Question_50
     {
         static void Main(string[] args)
         {
+            Console.ForegroundColor = ConsoleColor.Blue;
             Console.WriteLine("x*x+3*x-4");
             Console.WriteLine("(x+y)*z");
             Console.WriteLine("(x+3*y)/(2*y-x)");
@@ -13,9 +14,10 @@ namespace Question_50
             Console.WriteLine("(x+y)/7");
             Console.WriteLine("2*b*c*3");
             Console.WriteLine("x=(3*y)/(5-z)");
-            Console.WriteLine("z=area*sqrt(area)");
+            Console.WriteLine("z=area*Math.Sqrt(area)");
             Console.WriteLine("(x+32)/(y-32)-(x-2*y)");
-            Console.WriteLine("res = (x*i*j*k+pow(k,9))/(7*i*k-5*sqrt(j+k))");
+            Console.WriteLine("res = (3*i*j*k+Math.Pow(k,9))/(7*i*k-5*Math.Sqrt(j+k))");
+            
         }
     }
 }
